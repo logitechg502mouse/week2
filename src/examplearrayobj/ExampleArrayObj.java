aeedce65eb73c8699b6dafb2b56ecb0b5277937f
logitechg502mouse <lader@sheridancollege.ca>
2022-01-19 14:08:16 -0500

    version 1 jan 19 2021

---
+++ src/examplearrayobj/ExampleArrayObj.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examplearrayobj;
+
+/**
+ *array of objects - Student class - encapsulation 
+ * @author gen3r
+ */
+public class ExampleArrayObj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int [] list = new int[3] this is a array for primitive type, this will not work for objects
+        
+        Student[] list = new Student[3]; //aray of objects / array of object of Student class
+        Student s1 = new Student();
+        s1.setName("burh");
+        s1.setAge(23);
+        
+        
+       Student s2 = new Student();
+       s2.setName("uwu");
+       s2.setAge(189);
+       //save objects in array
+       
+       
+       list [0] = s1;
+       list [1] = s2;
+       
+       
+       
+       
+       for(int i = 0; i <2; i++){
+           
+//           System.out.println(list[i]);
+           //this will print out the address in the array 
+           System.out.println(list[i].getName() +": " + list[i].getAge());
+           //to solve that we do the dot(.) notation and reference the getters to be printed
+       }
+       
+       
+       
+       
+       
+    }
+    
+}
---
+++ src/examplearrayobj/Student.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examplearrayobj;
+
+/**
+ *define student information: Name and Age = the defining of data is 
+ * called model - raw data and method
+ * 
+ * @author gen3r
+ * @version 1.0 jdk-17
+ */
+public class Student {
+    private String name;//when able declaring anby variable, do private - encapsulation principle, *we are going to ingore that due to time(158)
+    private int age;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+   
+  
+       
+    
+}
+    
+
+
